9b77f4ed4fe219985a264fcdfe14776ea84888c3
Carlos <124552440+CarlitosJr66@users.noreply.github.com>
2024-04-07 14:20:17 -0500

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import CardDetails from './views/CardDetails.vue'
+import LoginPage from './views/LoginPage.vue'
+import SettingsPage from './views/SettingsPage.vue'
+
+import HomePage from '@/views/HomePage.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/login', name: 'LoginPage', component: LoginPage },
+  { path: '/settings', name: 'SettingsPage', component: SettingsPage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
